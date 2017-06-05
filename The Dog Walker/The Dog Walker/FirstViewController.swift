f0a526b562ee5c73d3050b0adc14cd3b3f3f152f
Scott O'Hara <esohara@fullsail.edu>
2017-06-04 21:50:26 -0400

    inital commit

---
+++ The Dog Walker/The Dog Walker/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  The Dog Walker
+//
+//  Created by Scott O'Hara on 6/4/17.
+//  Copyright © 2017 Scott O'Hara. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ The Dog Walker/The Dog WalkerTests/The_Dog_WalkerTests.swift
@@ -0,0 +1,36 @@
+//
+//  The_Dog_WalkerTests.swift
+//  The Dog WalkerTests
+//
+//  Created by Scott O'Hara on 6/4/17.
+//  Copyright © 2017 Scott O'Hara. All rights reserved.
+//
+
+import XCTest
+@testable import The_Dog_Walker
+
+class The_Dog_WalkerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
