1b57decf6005b20e271a623379f7c70bb3a7908a
Scott O'Hara <esohara@fullsail.edu>
2017-06-08 14:25:12 -0400

    fixing gitignore

--- The Dog Walker/The Dog Walker/LoginViewController.swift
+++ The Dog Walker/The Dog Walker/LoginViewController.swift
@@ -16,6 +16,8 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    
+    
     /*
     //MARK: -- Segue
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
