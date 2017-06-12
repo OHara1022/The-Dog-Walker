54fe090cdc7ce06b72944929e9e2c485677261b9
Scott O'Hara <esohara@fullsail.edu>
2017-06-12 15:41:35 -0400

    added imageview to pet registration

---
+++ The Dog Walker/The Dog Walker/PetRegisterViewController.swift
@@ -0,0 +1,49 @@
+//
+//  PetRegisterViewController.swift
+//  The Dog Walker
+//
+//  Created by Scott O'Hara on 6/12/17.
+//  Copyright © 2017 Scott O'Hara. All rights reserved.
+//
+
+import UIKit
+
+class PetRegisterViewController: UIViewController {
+    
+    
+    @IBAction func addImage(_ sender: UIButton) {
+    
+        let alert = UIAlertController(title: title, message: "HELLO", preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(alertAction)
+        present(alert, animated: true)
+    }
+    
+    @IBAction func backTest(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
